bc7ed5c171cc324445b6a4baa577cb4cdb7722d3
Lucas Wing <lucas.wing@aol.com>
2021-02-08 12:29:02 -0500

    Added better metrics, and arg handline

--- file-encrypter.py
+++ file-encrypter.py
@@ -20,6 +20,7 @@ import argparse
 
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor, wait
+from threading import Event, Thread
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
@@ -114,8 +115,8 @@ def decrypt_bytes(key, source, decode=True):
 #Functions
 #=============
 
-def _read_file(path):
-    with open(path, 'rb') as ifp:
+def _read_file(path, mode='rb'):
+    with open(path, mode) as ifp:
         return ifp.read()
 
 def _write_file(path, content):
@@ -130,13 +131,49 @@ def _get_parent(filepath):
 
 def _get_num_files_in_dir(filepath):
     numFiles = 0
+    numDirs = 0
     for root, subdirs, subfiles in os.walk(filepath):
         for subfile in subfiles:
             numFiles = numFiles + 1
-    return numFiles
+        for subdir in subdirs:
+            numDirs = numDirs + 1
+    return numFiles, numDirs
 
 #Classes
 #=============
+
+class _InvalidPasswordError(Exception):
+    """Raised when password is invalid"""
+    pass
+
+class _RepeatedTimer:
+
+    """
+    Repeat `function` every `interval` seconds.
+    https://stackoverflow.com/a/33054922/11381698
+    """
+
+    def __init__(self, interval, function, *args, **kwargs):
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.start = time.time()
+        self.event = Event()
+        self.thread = Thread(target=self._target)
+        self.thread.start()
+
+    def _target(self):
+        while not self.event.wait(self._time):
+            self.function(*self.args, **self.kwargs)
+
+    @property
+    def _time(self):
+        return self.interval - ((time.time() - self.start) % self.interval)
+
+    def stop(self):
+        self.event.set()
+        self.thread.join()
 
 class _FileCreation:
     """
@@ -163,6 +200,15 @@ class _DataStore:
         self._filepath = filepath
         self._lock = threading.Lock()
         self._store = self._load_store()
+    
+    def save_value(self, key, value):
+        with self._lock:
+            self._store[key] = value
+    
+    def get_value(self, key):
+        with self._lock:
+            if key in self._store:
+                return self._store[key]
     
     def build_store(self, key, empty_collection):
         with self._lock:
@@ -193,59 +239,72 @@ class _Metrics:
     Manages metric information for the encryption process
     """
     def __init__(self, folder):
+        self._folder =  folder[len(EXT_PATH):] if folder.startswith(EXT_PATH) else folder
         self._startTime = None
         self._filesProcessed = 0
-        self._totalFiles = _get_num_files_in_dir(folder)
+        self._fileNamesProcessed = 0
+        self._totalFiles, self._totalDirs = _get_num_files_in_dir(folder)
+        self._totalFilesAndDirs = self._totalFiles + self._totalDirs
         self._lock = threading.Lock()
-        self._timer = None
+        self._sched = None
     
-    def start(self):
+    def start(self, method):
         self._startTime = time.time()
+        self._sched = _RepeatedTimer(1, method)
+    
+    def stop(self):
+        self._sched.stop()
     
     # Metric calcs
     #============================
     
     def process_filename(self, filename):
+        with self._lock:
+            self._fileNamesProcessed = self._fileNamesProcessed + 1
         logging.debug('Processed filename: ' + filename)
     
     def process_file(self, filename):
         with self._lock:
             self._filesProcessed = self._filesProcessed + 1
         logging.debug('Processed file: ' + filename)
-        self.print_state()
     
-    def check_state(self):
+    def check_state(self, metric, metricName):
+        totalFiles = self._totalFiles if metricName == 'FILES_PROCESSED' else self._totalFilesAndDirs
         return  {
-                    'TIME_RUNNING (seconds)' : str(int(time.time() - self._startTime)),
-                    'FILES_PROCESSED' : self._filesProcessed,
-                    'TOTAL_FILES' : self._totalFiles,
-                    'PERCENT_COMPLETE' : int((self._filesProcessed / self._totalFiles) * 100) if self._totalFiles > 0 else 0
+                    'FOLDER' : self._folder,
+                    'TOTAL_TIME_RUNNING (seconds)' : str(int(time.time() - self._startTime)),
+                    metricName : metric,
+                    'TOTAL_FILES' : totalFiles,
+                    'PERCENT_COMPLETE' : int((metric / totalFiles) * 100) if totalFiles > 0 else 0
                 }
 
-    def print_state(self):
-        logging.info(str(self.check_state()))
+    def print_state_files(self):
+        logging.info(str(self.check_state(self._filesProcessed, 'FILES_PROCESSED')))
+    
+    def print_state_filenames(self):
+        logging.info(str(self.check_state(self._fileNamesProcessed, 'FILENAMES_PROCESSED')))
     
 # Runner
 #=================================================================
 
-class _RecursiveFileEncryptor:
+class RecursiveFileEncryptor:
     """
     Main class to contain all necessary objects/functions
     folder_location - location to encrypt/decrypt all files inside
     password - used for the encryption process
-    max_threads - number of threads to use. (generally just enough to max disk usage)
-                  note: this can significantly slow down computer operations, so reduce threads if needed
+    max_threads - number of threads to use.
     file_buffer_size - number of bytes to use (needs to be multiple of 16).
                   note: higher number here == more memory usage, but faster processing
                   note: passing in less than 1 calculates a default value.
     """
     METADATA_FILE = '.encrypted_file_map'
     
-    def __init__(self, folder_location, password, max_threads=multiprocessing.cpu_count(), file_buffer_size=0):
+    def __init__(self, folder_location, password, verifyPassword, max_threads=multiprocessing.cpu_count(), file_buffer_size=0):
+        self.verifyPassword = verifyPassword
         self.folder_location = EXT_PATH + str(Path(str(folder_location)))
         self.password = str(password)
         self.file_buffer_size = int(file_buffer_size) if int(file_buffer_size) > 0 else self._calc_default_buffer(int(max_threads))
-        self.metadata_file_location = os.path.join(self.folder_location, _RecursiveFileEncryptor.METADATA_FILE)
+        self.metadata_file_location = os.path.join(self.folder_location, RecursiveFileEncryptor.METADATA_FILE)
         self.encrypt_files = not(os.path.exists(self.metadata_file_location))
         self.thread_executor = ThreadPoolExecutor(max_workers=int(max_threads))
         self.fileCreation = _FileCreation()
@@ -262,14 +321,31 @@ class _RecursiveFileEncryptor:
             self._process_folder()
     
     def _process_folder(self):
-        self._metrics.start()
+        """
+        encrypt/decrypt the filenames/dirs and files themselves
+        filenames must go first, so the FILENAME_TO_ENCRYPTION_TYPE map can be safe.
+        """
+        if self.verifyPassword:
+            self._verify_password(self.password)
+
         if self.encrypt_files:
-            self._walk_encrypt_files(self.folder_location)
-            self._walk_encrypt_names(self.folder_location)
+            self._handle_encrypt_filenames()
+            self._handle_encrypt_files()
         else:
-            self._walk_encrypt_names(self.folder_location)
-            self._walk_encrypt_files(self.folder_location)
+            self._handle_encrypt_files()
+            self._handle_encrypt_filenames()
+
         self.datastore.save_store(not(self.encrypt_files))
+    
+    def _handle_encrypt_files(self):
+        self._metrics.start(self._metrics.print_state_files)
+        self._walk_encrypt_files(self.folder_location)
+        self._metrics.stop()
+    
+    def _handle_encrypt_filenames(self):
+        self._metrics.start(self._metrics.print_state_filenames)
+        self._walk_encrypt_names(self.folder_location)
+        self._metrics.stop()
     
     # Recursively walk directories
     #=================================================================
@@ -278,7 +354,7 @@ class _RecursiveFileEncryptor:
         futures = []
         for root, subdirs, subfiles in os.walk(file_input):
             for subfile in subfiles:
-                if subfile != _RecursiveFileEncryptor.METADATA_FILE:
+                if subfile != RecursiveFileEncryptor.METADATA_FILE:
                     futures.append(self.thread_executor.submit(self._process_file, os.path.join(root, subfile)))
         wait(futures)
     
@@ -286,7 +362,7 @@ class _RecursiveFileEncryptor:
         if os.path.isdir(filepath):
             for file_input in os.listdir(filepath):
                 self._walk_encrypt_names(os.path.join(filepath, file_input))
-        if _get_filename(filepath) != _RecursiveFileEncryptor.METADATA_FILE and filepath != self.folder_location:
+        if _get_filename(filepath) != RecursiveFileEncryptor.METADATA_FILE and filepath != self.folder_location:
             self._process_file_name(filepath)
     
     # Encrypt/Decrypt files and filenames
@@ -295,17 +371,20 @@ class _RecursiveFileEncryptor:
     def _process_file(self, file_input):
         tmpFilename = os.path.join(_get_parent(file_input), self.fileCreation.get_filename()) + '.tmp'
         if self.encrypt_files:
-            self.filename_to_encryptionType_dict[file_input] = encrypt_file(file_input, tmpFilename, self.password, self.file_buffer_size)
+            file_input_enc = file_input + ENCRYPTED_EXT
+            os.rename(file_input, file_input_enc)
+            self.filename_to_encryptionType_dict[file_input_enc] = encrypt_file(file_input_enc, tmpFilename, self.password, self.file_buffer_size)
         else:
-            if file_input.rsplit('.')[1] == ENCRYPTED_EXT:
+            if file_input[-len(ENCRYPTED_EXT):] == ENCRYPTED_EXT:
                 decrypt_file(file_input, tmpFilename, self.password, self.file_buffer_size, self.filename_to_encryptionType_dict[file_input]) 
+                os.rename(file_input, file_input[:-len(ENCRYPTED_EXT)])
         self._metrics.process_file(file_input) 
 
     def _process_file_name(self, file_input):
         input_name = _get_filename(file_input)
         output_file_name = None
         if self.encrypt_files:
-            output_file_name = self.fileCreation.get_filename() + ENCRYPTED_EXT
+            output_file_name = self.fileCreation.get_filename()
             self.uuid_to_filename_dict[output_file_name] = encrypt_bytes(str.encode(self.password), str.encode(input_name), False)
         else:
             if input_name in self.uuid_to_filename_dict:
@@ -322,7 +401,18 @@ class _RecursiveFileEncryptor:
     def _calc_default_buffer(self, threadCount, memoryMultiplier = 0.75):
         memory_bytes_per_thread = (psutil.virtual_memory().available * memoryMultiplier) // threadCount
         return int(memory_bytes_per_thread - (memory_bytes_per_thread % 16))
-
+    
+    def _verify_password(self, password):
+        if self.encrypt_files:
+            encryptedPassword = encrypt_bytes(str.encode(self.password), str.encode(_DataStore.PASSWORD_VERIFIER_KEY), False)
+            self.datastore.save_value(_DataStore.PASSWORD_VERIFIER_KEY, encryptedPassword)
+        else:
+            encryptedPassword = self.datastore.get_value(_DataStore.PASSWORD_VERIFIER_KEY)
+            if encryptedPassword:
+                decryptedPassword = decrypt_bytes(str.encode(self.password), encryptedPassword, False).decode()
+                if decryptedPassword == _DataStore.PASSWORD_VERIFIER_KEY:
+                    return None
+            raise _InvalidPasswordError
 # ================================================================
 #
 # Main
@@ -331,13 +421,23 @@ class _RecursiveFileEncryptor:
 
 def main():
     '''
-    Takes input for folder + password
+    Takes input for password + folder(s)
     Ex.  file-encrypter.py TEST123 E:/testtttt/testFolder "E:/testtttt/Everything Needed"
     Ex.  file-encrypter.py TEST123 "E:/testtttt/Everything Needed"
-    - I still need to make this be able to take in a password file
+    Ex.  file-encrypter.py --pwdfile E:/testtttt/pwd.txt E:/testtttt/testFolder
     '''
     parser = argparse.ArgumentParser(description='Encrypts folder contents with provided password.')
+
+    #optional arg to specify password field is a file which has password inside it
+    parser.add_argument("--pwdfile", action='store_true', 
+                        help="uses password stored in file (to avoid cmdline history)")
+
+    #optional arg to enable password verification
+    parser.add_argument("--verify", action='store_true', 
+                        help="""Checks if password is correct before decrypting. Must have been run on initial encrypt.
+                              (keep in mind, this does provide an avenue for brute forcing)""")
     
+    #arg for password
     def _check_password(password):
         if len(password) <= 0:
             raise argparse.ArgumentTypeError("%s is an invalid password" % password)
@@ -346,6 +446,7 @@ def main():
     parser.add_argument("password", type=_check_password, 
                         help="password which will be used for the encryption/decryption")
     
+    #arg for folder(s)
     def _check_folder(folder):
         if not(os.path.isdir(folder)):
             raise argparse.ArgumentTypeError("%s is an invalid folder" % folder)
@@ -356,10 +457,23 @@ def main():
 
     args = parser.parse_args()
 
+    #handle specifying password is a file
+    if args.pwdfile:
+        if os.path.exists(args.password):
+            args.password = _read_file(args.password,"r+")
+        else:
+            raise argparse.ArgumentTypeError("%s does not exist" % args.password)
+    
+    #logging setup
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.getLogger('apscheduler').setLevel(logging.WARNING)
 
+    #run encrypt/decrypt on all folders
     for folder in args.folders:
-        _RecursiveFileEncryptor(os.path.abspath(folder), args.password).run()
+        try:
+            RecursiveFileEncryptor(os.path.abspath(folder), args.password, args.verify).run()
+        except _InvalidPasswordError:
+            print("The password you entered is invalid for: " + folder)
 
 
 if __name__ == '__main__':
